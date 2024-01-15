97cc72c27ea269674fe54b1039e69c4eb3091aee
WilliamASease <seasewa@appstate.edu>
2024-01-15 01:17:14 -0500

    lots of work

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
   const { imageDefinitions, relPathsForPreload } = useImageDefinitions();
 
   const blackBorder = { border: "solid black 5px" };
+  const diceStyle = { border: "solid black 5px", borderRadius: 25 };
 
   return (
     <FlexBox
@@ -45,11 +46,11 @@ function App() {
         <SiteHorizontalRule />
         <SiteBody>
           <FlexBox
-            style={{ height: "50%", border: "solid red 1px" }}
+            style={{ height: "50%", border: "solid black 2px" }}
             orientation="row"
           >
             <FlexBox
-              style={{ width: "50%", border: "solid blue 1px" }}
+              style={{ width: "50%", border: "solid black 2px" }}
               orientation="column"
             >
               <InlineImage
@@ -57,50 +58,104 @@ function App() {
                 {...imageDefinitions.Fight}
               />
               <FlexBox style={{ flexGrow: 1 }} orientation={"column"}>
-                <FlexBox fullWidth orientation="row" style={{ height: "50%" }}>
-                  <InlineImage noDiv {...imageDefinitions.atk1} />
-                  <InlineImage noDiv {...imageDefinitions.atk2} />
+                <FlexBox
+                  fullWidth
+                  orientation="row"
+                  style={{
+                    height: "50%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <InlineImage
+                    style={diceStyle}
+                    noDiv
+                    {...imageDefinitions.atk1}
+                  />
+                  <InlineImage
+                    style={diceStyle}
+                    noDiv
+                    {...imageDefinitions.atk2}
+                  />
                 </FlexBox>
-                <FlexBox fullWidth orientation="row" style={{ height: "50%" }}>
-                  <InlineImage noDiv {...imageDefinitions.def1} />
-                  <InlineImage noDiv {...imageDefinitions.def2} />
-                  <InlineImage noDiv {...imageDefinitions.def3} />
+                <FlexBox
+                  fullWidth
+                  orientation="row"
+                  style={{
+                    height: "50%",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <InlineImage
+                    style={diceStyle}
+                    noDiv
+                    {...imageDefinitions.def1}
+                  />
+                  <InlineImage
+                    style={diceStyle}
+                    noDiv
+                    {...imageDefinitions.def2}
+                  />
+                  <InlineImage
+                    style={diceStyle}
+                    noDiv
+                    {...imageDefinitions.def3}
+                  />
                 </FlexBox>
               </FlexBox>
               <InlineImage
-                style={{ width: isMobile ? "100%" : "50%", ...blackBorder }}
+                style={{ width: isMobile ? "90%" : "50%", ...blackBorder }}
                 {...imageDefinitions.Roll}
               />
               <InlineImage
-                style={{ width: isMobile ? "100%" : "50%", ...blackBorder }}
+                style={{ width: isMobile ? "90%" : "50%", ...blackBorder }}
                 {...imageDefinitions.RollAll}
               />
             </FlexBox>
             <FlexBox
-              style={{ width: "50%", border: "solid blue 1px" }}
+              style={{ width: "50%", border: "solid black 2px" }}
               orientation="column"
             >
               <InlineImage
                 style={{ width: "100%" }}
                 {...imageDefinitions.CombatLog}
               />
+              <div style={{ flexGrow: 1, overflowY: "scroll" }}>
+                {[
+                  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+                  19, 20,
+                ].map((i) => (
+                  <FlexBox orientation="row">
+                    <SiteText value="Log Entry" />
+                  </FlexBox>
+                ))}
+              </div>
             </FlexBox>
           </FlexBox>
           <FlexBox
-            style={{ height: "50%", border: "solid red 1px" }}
+            style={{ height: "50%", border: "solid black 2px" }}
             orientation="row"
           >
             <FlexBox
-              style={{ width: "50%", border: "solid blue 1px" }}
+              style={{ width: "50%", border: "solid black 2px" }}
               orientation="column"
             >
               <InlineImage
                 style={{ width: "100%" }}
                 {...imageDefinitions.ArmySize}
               />
+              <FlexBox style={{ flexGrow: 1 }} orientation="column">
+                <FlexBox style={{ height: "50%" }} orientation="row">
+                  Set Red
+                </FlexBox>
+                <FlexBox style={{ height: "50%" }} orientation="row">
+                  Set Blue
+                </FlexBox>
+              </FlexBox>
             </FlexBox>
             <FlexBox
-              style={{ width: "50%", border: "solid blue 1px" }}
+              style={{ width: "50%", border: "solid black 2px" }}
               orientation="column"
             >
               <InlineImage
